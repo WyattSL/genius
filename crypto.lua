8e792e9f8e88c2b154a35f0f4cc2e2d9975efa05
Wyatt Lipscomb <35582368+WyattSL@users.noreply.github.com>
2020-11-16 11:19:12 -0500

    Update crypto.lua

--- crypto.lua
+++ crypto.lua
@@ -1,5 +1,14 @@
+swap = function(t, v1, v2)
+ z = t[v1]
+ t[v1] = t[v2]
+ t[v2] = z
+end function
 a = {}
+for i in range(0,255)
+  a[i] = i
+end for
 b = 0
 for c in a
-  b = (b + a[i] + K[i mod k_len]) mod 256
+  b = (b + a[i] + K[i % k.len]) % 256
+  swap(a,i,b)
 end for
